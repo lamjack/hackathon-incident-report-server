044ac7bc63fe58fd657db651d953f92a11b0be7f
jack <linjue@wilead.com>
2015-11-28 15:20:23 +0800

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Wiz\ResqueBundle\WizResqueBundle(),
             new Acme\AppBundle\AcmeAppBundle(),
+            new Kreait\FirebaseBundle\KreaitFirebaseBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
